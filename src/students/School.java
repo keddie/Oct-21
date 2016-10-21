7e153b21bc1c4fe35225fd534b304aaf8d817faf
SimonHGR <simon@dancingcloudservices.com>
2016-10-21 11:26:38 -0400

    FlatMap example

--- src/students/School.java
+++ src/students/School.java
@@ -1,25 +1,65 @@
 package students;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class School {
 
 	public static void main(String[] args) {
-		List<Student> roster = Arrays.asList(
-				Student.ofNamesGpaCourses("Fred", "Jones", 3.2F, "Math", "Physics"),
+		List<Student> roster = Arrays.asList(Student.ofNamesGpaCourses("Fred", "Jones", 3.2F, "Math", "Physics"),
 				Student.ofNamesGpaCourses("Jim", "Smith", 3.4F, "Math", "Physics", "Chemistry"),
 				Student.ofNamesGpaCourses("Shiela", "Pearl", 2.9F, "Math"),
 				Student.ofNamesGpaCourses("Alice", "Barnett", 2.2F, "Chemistry", "Physics", "Biology"),
 				Student.ofNamesGpaCourses("Fred", "Jones", 3.1F, "Math"),
 				Student.ofNamesGpaCourses("Fred", "Blair", 3.7F, "Math", "Art"),
-				Student.ofNamesGpaCourses("Jenny", "Smith", 3.1F, "Art", "French", "Geography")
-				);
+				Student.ofNamesGpaCourses("Jenny", "Smith", 3.1F, "Art", "French", "Geography"));
+
+		Predicate<Student> smart = s -> s.getGpa() > 3.3F;
+		roster.stream()
+		.filter(smart)
+		.map(s -> s.getLastName() + " scored " + s.getGpa())
+		.forEach(System.out::println);
+		System.out.println("--------------");
+
+		roster.stream()
+		.filter(smart.negate())
+		.forEach(s -> System.out.println("Smart: " + s));
+		System.out.println("--------------");
+
+		roster.stream()
+		.sorted((a,b) -> a.getCourses().size() - b.getCourses().size())
+		.forEach(System.out::println);
+		System.out.println("--------------");
+
+		roster.stream()
+		.sorted(Comparator.<Student>comparingInt(s->s.getCourses().size()))
+		.forEach(System.out::println);
+		System.out.println("--------------");
 		
 		roster.stream()
-			.filter(s->s.getGpa()> 3.3F)
-			.map(s->s.getLastName() + " scored " + s.getGpa())
-			.forEach(System.out::println);
+		.max(Comparator.<Student>comparingDouble(s->s.getGpa()))
+		.ifPresent(s -> System.out.println("smartest is " + s));
+		System.out.println("--------------");
+		
+		Stream.<Student>empty()
+		.max(Comparator.<Student>comparingDouble(s->s.getGpa()))
+		.ifPresent(s -> System.out.println("smartest is " + s));
+		System.out.println("--------------");
+		
+		roster.stream()
+		.peek(System.out::println)
+//		.sorted(Comparator.<Student>comparingInt(s->s.getCourses().size()))
+		.findAny()
+		;
+		System.out.println("--------------");
+		
+		roster.stream()
+		.flatMap(s-> Stream.of(s.getFirstName(), s.getLastName()))
+		.forEach(System.out::println);
+		System.out.println("--------------");
 	}
 
 }
